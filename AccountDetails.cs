a6392f99cb61e14ba4c825d4cf29409a44ad1e31
Bera <poulami.bera@capgemini.com>
2024-01-18 16:41:49 +0530

    first commit of Project

---
+++ AccountDetails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class AccountDetails
+    {
+        public void GetData()
+        {
+            Console.WriteLine("enter your balance");
+            double balance = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter your account type");
+            string accountType = Console.ReadLine();
+            Console.WriteLine("enter your deposit amount");
+            double amount = Convert.ToDouble(Console.ReadLine());
+            balance += amount;
+            Console.WriteLine($"new balance is :{balance}");
+        }
+    }
+}
---
+++ BookRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class BookRepository
+    {
+        public List<Book> Books { get; set; }
+
+        public void DisplayallBooks()
+        {
+            foreach(Book b in Books)
+            {
+                Console.WriteLine($"{b.BookId} {b.Title} {b.Price} {b.Publisher}");
+            }
+        }
+        public void DisplayBookByPublisher(string publisher)
+        {
+            foreach (Book b in Books)
+            {
+                if (b.Publisher == publisher)
+                {
+                    Console.WriteLine($"{b.BookId} {b.Title} {b.Price} {b.Publisher}");
+                }
+            }
+        }
+    }
+}
